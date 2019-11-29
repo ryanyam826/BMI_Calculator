4a33222a1fbc9127a40d17385e3924fbd64b37e8
ryanyam826 <yamseng999@gmail.com>
2019-11-29 14:50:56 +0800

    initial commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,13 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.view.Gravity
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,22 +18,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnCal.setOnClickListener(){
-            val weight = weightValue.text.toString().toDouble()
-            val height = heightValue.text.toString().toDouble()
-            val bmi = weight / (height * height)
+            try{
+                val weight = weightValue.text.toString().toDouble()
+                val height = heightValue.text.toString().toDouble()
+                val bmi = weight / (height * height)
 
 
-            if (bmi < 18.5){
-                imageView.setImageResource(R.drawable.under)
-                bmiResult.text = String.format("%.2f",bmi) + " (Underweight)"
-            }else if (bmi <= 24.9 ){
-                imageView.setImageResource(R.drawable.normal)
-                bmiResult.text = String.format("%.2f",bmi) + " (Normal)"
-            }else if(bmi > 25){
-                imageView.setImageResource(R.drawable.over)
-                bmiResult.text = String.format("%.2f",bmi) + " (Overweight)"
+                if (bmi < 18.5) {
+                    imageView.setImageResource(R.drawable.under)
+                    bmiResult.text = String.format("%.2f", bmi) + " (Underweight)"
+                } else if (bmi <= 24.9) {
+                    imageView.setImageResource(R.drawable.normal)
+                    bmiResult.text = String.format("%.2f", bmi) + " (Normal)"
+                } else if (bmi > 25) {
+                    imageView.setImageResource(R.drawable.over)
+                    bmiResult.text = String.format("%.2f", bmi) + " (Overweight)"
+                }
+            }catch (ex:Exception){
+                val toast: Toast = Toast.makeText(this,"Invalid Input",Toast.LENGTH_LONG)
+
+                toast.setGravity(Gravity.CENTER,0,0)
+                toast.show()
             }
-
 
         }
 
